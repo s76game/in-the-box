c4fc30b1917026cb860e778150103697b6593d7a
appleguy1999 <ryan.cobelli@gmail.com>
2014-06-15 16:43:12 -0400

    Initial Commit

---
+++ Bounce ball/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bounce ball
+//
+//  Created by Ryan Cobelli on 6/15/14.
+//  Copyright (c) 2014 Rybel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Bounce ballTests/Bounce_ballTests.m
@@ -0,0 +1,39 @@
+//
+//  Bounce_ballTests.m
+//  Bounce ballTests
+//
+//  Created by Ryan Cobelli on 6/15/14.
+//  Copyright (c) 2014 Rybel. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Bounce_ballTests : XCTestCase
+
+@end
+
+@implementation Bounce_ballTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
